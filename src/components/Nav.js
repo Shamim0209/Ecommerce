38f75af2c2fb0ee755068e77dc08d1af1bfd1d99
Shamim0209 <meshamim800@gmail.com>
2024-02-08 22:41:25 +0530

    made add to cart button active

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -205,7 +205,7 @@ const Nav = () => {
           <li>
             <NavLink to="/cart" className="navbar-link cart-trolley--link">
               <FiShoppingCart className="cart-trolley" />
-              <span className="cart-total--item"> 10 </span>
+              {/* <span className="cart-total--item"> 10 </span> */}
             </NavLink>
           </li>
         </ul>
