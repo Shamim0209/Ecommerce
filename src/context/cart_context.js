9dcddd5e1f5ee9e11b4e12ab2948fe5bd6e3c3fd
Shamim0209 <meshamim800@gmail.com>
2024-02-26 23:49:35 +0530

    Cart page finished

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,13 +1,22 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import reducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
 
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("myCart");
+  const parsedData = JSON.parse(localCartData);
+  if (!Array.isArray(parsedData)) return [];
+
+  return parsedData;
+};
+
 const initialState = {
-  cart: [],
+  // cart: [],
+  cart: getLocalCartData(),
   total_item: "",
-  total_amount: "",
-  shipping_fee: 50000,
+  total_price: "",
+  shipping_fee: 10000,
 };
 
 const CartProvider = ({ children }) => {
@@ -17,12 +26,46 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
 
+  // increment and decrement the product
+
+  const setDecrease = (id) => {
+    dispatch({ type: "SET_DECREMENT", payload: id });
+  };
+
+  const setIncrement = (id) => {
+    dispatch({ type: "SET_INCREMENT", payload: id });
+  };
+
+  // to remove the individual item from cart
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
 
+  // to clear the cart
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  // to add the data in localStorage
+  // get vs set
+
+  useEffect(() => {
+  
+    dispatch({ type: "CART_ITEM_PRICE_TOTAL" });
+
+    localStorage.setItem("myCart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
+    <CartContext.Provider
+      value={{
+        ...state,
+        addToCart,
+        removeItem,
+        clearCart,
+        setDecrease,
+        setIncrement,
+      }}>
       {children}
     </CartContext.Provider>
   );
